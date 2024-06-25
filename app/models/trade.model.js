d7ef1ac0ea63775527d55a7879918a07382d9a94
Nandnigarg <gargnandni64@gmail.com>
2024-06-25 17:45:21 +0530

    First file

---
+++ app/models/trade.model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const tradeSchema = new mongoose.Schema({
+  utcTime: Date,
+  operation: String,
+  market: String,
+  baseCoin: String,
+  quoteCoin: String,
+  buySellAmount: Number,
+  price: Number
+});
+
+module.exports = mongoose.model('Trade', tradeSchema);
